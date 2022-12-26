55792c260a21589983fa80ddb8f9b3fc626975ee
The Star Gazer <20520417@gm.uit.edu.vn>
2022-12-26 21:12:34 +0700

    Them mvvm

--- OOP/QLBaiDoXe/QLBaiDoXe/DS_NhanVien.xaml.cs
+++ OOP/QLBaiDoXe/QLBaiDoXe/DS_NhanVien.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using QLBaiDoXe.ViewModel;
 
 namespace QLBaiDoXe
 {
@@ -22,6 +23,7 @@ namespace QLBaiDoXe
         public DS_NhanVien()
         {
             InitializeComponent();
+            this.DataContext = new DSNhanVienViewModel();
         }
     }
 }
