51cae497016d9c80456a5605dfee61609469c213
The Star Gazer <20520417@gm.uit.edu.vn>
2022-12-29 14:37:56 +0700

    Tra cuu xe theo ngay

--- OOP/QLBaiDoXe/QLBaiDoXe/TraCuuXe.xaml.cs
+++ OOP/QLBaiDoXe/QLBaiDoXe/TraCuuXe.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ControlzEx.Standard;
+using QLBaiDoXe.DBClasses;
+using QLBaiDoXe.ParkingLotModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +26,84 @@ namespace QLBaiDoXe
         public TraCuuXe()
         {
             InitializeComponent();
+        }
+        private void Nullify()
+        {
+            cbxDay.Text = null;
+            cbxMonth.Text = null;
+            cbxYear.Text = null;
+        }
+        private bool CheckInput()
+        {
+
+            int testDay;
+            int.TryParse(cbxDay.Text, out testDay);
+
+
+            int testMonth;
+            int.TryParse(cbxMonth.Text, out testMonth);
+
+
+            int testYear;
+            int.TryParse(cbxYear.Text, out testYear);
+
+
+            if (testMonth == 2)
+            {
+                if (DateTime.IsLeapYear(testYear))
+                {
+                    if (testDay > 29)
+                    {
+                        MessageBox.Show("Bạn đã nhập ngày không phù hợp");
+                        Nullify();
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (testDay > 28)
+                    {
+                        MessageBox.Show("Bạn đã nhập ngày không phù hợp");
+                        Nullify();
+                        return false;
+                    }
+                }
+            }
+            if (testMonth == 4 || testMonth == 6 || testMonth == 9 || testMonth == 11)
+                if (testDay > 30)
+                {
+                    MessageBox.Show("Bạn đã nhập ngày không phù hợp");
+                    Nullify();
+                    return false;
+                }
+            return true;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            List<Vehicle> result = new List<Vehicle>();
+            //if (TimePicker.Text != null)
+            //{
+            //    DateTime datesearch = new DateTime(int.Parse(cbxYear.Text), int.Parse(cbxMonth.Text), int.Parse(cbxDay.Text), int.Parse(TimePicker.Text),0,0);
+            //    result = ParkingVehicle.SearchVehicle_TimeIn_DateAndHour(datesearch);
+            //}    
+            //else
+            //{
+            //    DateTime datesearch = new DateTime(int.Parse(cbxYear.Text), int.Parse(cbxMonth.Text), int.Parse(cbxDay.Text));
+            //    result = ParkingVehicle.SearchVehicle_TimeIn_DateOnly(datesearch);
+
+            //}
+            DateTime datesearch = new DateTime(int.Parse(cbxYear.Text), int.Parse(cbxMonth.Text), int.Parse(cbxDay.Text));
+            result = ParkingVehicle.SearchVehicle_TimeIn_DateOnly(datesearch);
+            if (CheckInput() == false) { return; }
+              
+            if (result.Count == 0)
+            {
+                MessageBox.Show("Ngày bạn đã nhập không có xe");
+            }
+            lvResult.ItemsSource = null;
+            lvResult.ItemsSource = result;
+
         }
     }
 }
