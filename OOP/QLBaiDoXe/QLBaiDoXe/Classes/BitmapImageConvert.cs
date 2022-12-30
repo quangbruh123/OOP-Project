e0dc49821bfb706782bf470f7eeba0f3213327a3
quangbruh123 <20520716@gm.uit.edu.vn>
2022-12-30 14:16:00 +0700

    Add classes file

---
+++ OOP/QLBaiDoXe/QLBaiDoXe/Classes/BitmapImageConvert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+
+namespace QLBaiDoXe.Classes
+{
+    static class BitmapImageConvert
+    {
+        static public Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            return new Bitmap(bitmapImage.StreamSource);
+        }
+    }
+}
